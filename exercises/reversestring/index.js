ec559c836f458792bcb20aa40dec53aed0099cbb
Marie Carter <marievcarter@gmail.com>
2019-03-10 18:21:54 -0400

    Completed reverse string.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// with simple for loop
+function reverse(str) {
+  let reversedStr = '';
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   reversedStr += str[i];
+  // }
+  // try to start using new syntax more
+  for (let char of str) {
+    reversedStr = char + reversedStr;
+  }
+  return reversedStr;
+}
+
+// with reduce
+// const reverse = str => {
+//   // reducer function
+//   let reducer = (accumulator, currentVal) => {
+//     return currentVal + accumulator;
+//   };
+//   // reduce takes a callback function for action and starting point
+//   return str.split('').reduce(reducer, '');
+// };
+
+// with reverse
+// const reverse = str => {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// };
 
 module.exports = reverse;
